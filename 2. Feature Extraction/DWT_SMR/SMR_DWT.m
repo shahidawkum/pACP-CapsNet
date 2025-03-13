9756fc518318719672ca3e479401cf63fffa791d
shahidawkum <60503977+shahidawkum@users.noreply.github.com>
2025-03-14 01:11:39 +0800

    Add files via upload

---
+++ 2. Feature Extraction/DWT_SMR/SMR_DWT.m
@@ -0,0 +1,36 @@
+clc
+clear all
+load('energy_20.mat');
+
+feature_train=[];
+
+
+[data, sequence] = fastaread('Test_Dataset.txt');
+
+Total_Seq_train=size(sequence,2);
+for i=1:(Total_Seq_train)
+     i
+    SEQ=sequence(i);
+    SEQ=cell2mat(SEQ);
+    P=SMR(SEQ,energy_20);
+	P=P';
+    P = uint8(255 * mat2gray(P));
+	
+    
+%%%%%%%%%%% SMR-DWT %%%%%%%%%%%%%%%%
+    FF=GetDWT(P);
+    SMR_DWT_features(i,:)=FF;
+        
+       
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%% SAVE FILES %%%%%%%%%%%%%%%%%%%%%%%%%
+SMR_DWT_features_ACP=[SMR_DWT_features];
+
+save SMR_DWT_features_ACP SMR_DWT_features_ACP;
+
+%%%% To Create CSV sheet for the data %%%%%%%%%
+   
+    csvwrite('SMR_DWT_features_ACP.csv',SMR_DWT_features_ACP);
+     
+
